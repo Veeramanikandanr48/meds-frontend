6e7b773048c81ba1b09576780dff0c944c9945d7
Veeramanikandan R <119089869+Veeramanikandanr48@users.noreply.github.com>
2024-08-03 23:19:03 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import payment from '../media/payment.png';
-import securePay from '../media/secure_pay.png';
 
 const Footer = () => {
   return (
     <footer className="bg-light text-dark text-center py-4">
       <div className="container mx-auto px-4">
-        <div className="grid grid-cols-1 md:grid-cols-5 gap-5 text-sm text-left">
-
-        <div className="copy-right-sec mt-1">
-          <p className="copy-right text-xs">Copyright © medsforhim.com. All rights reserved</p>
+        <div className="grid grid-cols-1 text-sm text-left">
+          {/* Copyright Section */}
+          <div className="copy-right-sec mt-1">
+            <p className="copy-right text-xs">Copyright © medsforhim.com. All rights reserved</p>
+          </div>
         </div>
       </div>
     </footer>
