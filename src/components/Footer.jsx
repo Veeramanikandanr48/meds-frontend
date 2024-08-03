15a222727e189a8bb76b7fad6493a63acaa39fbf
Veeramanikandan R <119089869+Veeramanikandanr48@users.noreply.github.com>
2024-08-03 22:41:54 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,12 +13,12 @@ const Footer = () => {
           <div className="text-sm">
             <ul className="list-none">
               <li>Home</li>
-              <li><Link to="/contact-us">Contact Us</Link></li> {/* Contact Us Link */}
+              <li><Link to="/contact-us">Contact Us</Link></li> {/* Contact Us Link
               <li>About Us</li>
             </ul>
           </div>
 
-          {/* Mobile Version (Hidden in Mobile View) */}
+          Mobile Version (Hidden in Mobile View)
           <div className="hidden md:block">
           <Link to="/contact-us" className="block w-75 border border-blue-500 bg-blue-500 text-white font-bold py-2 px-2 rounded-full text-center mt-4 hover:bg-blue-700 hover:border-blue-700">
         Contact Us
@@ -26,7 +26,7 @@ const Footer = () => {
           </div>
         </div>
 
-        {/* Copy Right Section */}
+        Copy Right Section */}
         <div className="copy-right-sec mt-1">
           <p className="copy-right text-xs">Copyright © budgetmeds.com. All rights reserved</p>
         </div>
