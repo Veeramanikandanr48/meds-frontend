eddb2c2309704577b3f71536c054c0043a287525
Veeramanikandan R <119089869+Veeramanikandanr48@users.noreply.github.com>
2024-08-03 23:13:20 +0530

    Update Carousel.jsx

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from "react";
+import { Link } from 'react-router-dom';
 import { Carousel } from "bootstrap"; // Import Bootstrap Carousel
 
 const CarouselComponent = () => {
