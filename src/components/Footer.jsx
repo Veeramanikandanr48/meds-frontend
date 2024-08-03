7030e9a0f05389d97979d158e4265569a4025a47
Veeramanikandan R <119089869+Veeramanikandanr48@users.noreply.github.com>
2024-08-03 22:42:25 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,11 +9,11 @@ const Footer = () => {
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-5 gap-5 text-sm text-left">
 
-          {/* Home, Contact Us, About Us */}
+          {/* Home, Contact Us, About Us
           <div className="text-sm">
             <ul className="list-none">
               <li>Home</li>
-              <li><Link to="/contact-us">Contact Us</Link></li> {/* Contact Us Link
+              <li><Link to="/contact-us">Contact Us</Link></li> Contact Us Link
               <li>About Us</li>
             </ul>
           </div>
