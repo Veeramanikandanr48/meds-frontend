fb7d3928dcd9e2a193f156a2f51e83f2a3f3148b
Veeramanikandan R <119089869+Veeramanikandanr48@users.noreply.github.com>
2024-11-24 15:56:57 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -22,8 +22,8 @@ const Sidebar = () => {
     fetchCategories();
   }, []);
 
-  const handleCategoryClick = (category) => {
-    setActiveCategory(category);
+  const handleCategoryClick = (categoryName) => {
+    setActiveCategory(categoryName);
   };
 
   return (
@@ -31,20 +31,33 @@ const Sidebar = () => {
       <h1 className="border-b py-2 h6">Categories List</h1>
       <ul className="flex flex-col text-xs gap-2 text-gray-600">
         {categories.map((category, index) => (
-          <li key={index} className="cursor-pointer flex items-center gap-1" onClick={() => handleCategoryClick(category)}>
+          <li 
+            key={index} 
+            className="cursor-pointer flex items-center gap-1" 
+            onClick={() => handleCategoryClick(category.name)}
+          >
             <img
               width="20"
               height="20"
-              src={`${category === activeCategory ? 'https://img.icons8.com/pulsar-gradient/48/add-folder.png':'https://img.icons8.com/pulsar-line/48/add-folder.png'}`}
+              src={`${category.name === activeCategory 
+                ? 'https://img.icons8.com/pulsar-gradient/48/add-folder.png'
+                : 'https://img.icons8.com/pulsar-line/48/add-folder.png'}`}
               alt="folder-invoices"
             />
-            <Link to={`/category/${category}`} className={category === activeCategory ? 'font-bold' : ''} style={{textDecoration: 'none', color: 'inherit'}}>
-              {category}
+            <Link 
+              to={`/category/${category.name}`} 
+              className={category.name === activeCategory ? 'font-bold' : ''} 
+              style={{textDecoration: 'none', color: 'inherit'}}
+            >
+              {category.name}
             </Link>
           </li>
         ))}
       </ul>
-      <Link to="/contact-us" className="block border border-blue-500 bg-blue-500 text-white font-bold py-2 px-4 rounded-full text-center mt-4 hover:bg-blue-700 hover:border-blue-700">
+      <Link 
+        to="/contact-us" 
+        className="block border border-blue-500 bg-blue-500 text-white font-bold py-2 px-4 rounded-full text-center mt-4 hover:bg-blue-700 hover:border-blue-700"
+      >
         Contact Us
       </Link>
       <h1 className="border-b py-2">Shipping option</h1>
@@ -54,4 +67,3 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
-// added
